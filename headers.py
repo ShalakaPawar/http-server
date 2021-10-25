e52d06c2745653501d696af7c194ba69bd0023f4
shalaka23 <pawarss19.comp@coep.ac.in>
2021-10-25 07:42:15 +0000

    Upload New File

---
+++ headers.py
@@ -0,0 +1,65 @@
+"""
+Content-Encoding : gzip/deflate/compress
+Accept-Encoding : gzip/deflate/compress/identity
+"""
+
+# Headers
+
+# request headers
+"""
+Accept-Charset
+Accept-Encoding
+Accept-Language
+Authorization
+Expect
+From
+Host
+If-Match
+If-Modified-Since
+If-None-Match
+If-Range
+If-Unmodified-Since
+Max-Forwards
+Proxy-Authorization
+Range
+Referer
+TE
+User-Agent
+
+"""
+
+# response headers
+"""
+Content-Type :
+Content-Encoding :
+Cache-Control : 
+Age
+
+ETag
+
+Location
+
+Proxy-Authenticate
+
+Retry-After
+
+Server
+
+Vary
+
+WWW-Authenticate
+
+"""
+
+# entity headers
+"""
+Allow : GET, PUT, HEAD
+Content-Encoding :
+Content-Language : 
+Content-Length : 
+Content-Location : 
+Content-MD5 :
+Content-Range :
+Content-Type :
+Expires : 
+"""
