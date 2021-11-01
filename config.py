55dab9b3d9db82cec37ff6287057be71153dc2a6
shalaka23 <pawarss19.comp@coep.ac.in>
2021-11-01 02:50:53 +0000

    commit

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 import sys
-import os
+import os, stat
 
-ServerRoot = str(Path().absolute())
+ServerRoot = str(Path().absolute())+'/'
+
+# fetch file location - get, head
+FetchFile = 'responsefiles'
+
+#store file location - post, put
+StoreFile = 'serverfiles'
 
 # port number of server
 PORT = 2000
@@ -30,6 +36,23 @@ AccesLog = ServerRoot + '/logfiles/accesslog.log'
 
 # format
 # clientIP date_time request_method response_status resp_message content-length 
+"""ErrorLogFormat "[%t] [%l] [pid %P] %F: %E: [client %a] %M"
+
+%a	Client IP address and port of the request
+%t	The current time
+%l	Loglevel of the message
+%P	Process ID of current process
+%F	Source file name and line number of the log call
+%M	The actual log message
+%E	APR/OS error status code and string
+"""
+
+#Log level:
+#crit		Critical Conditions.	"socket: Failed to get a socket, exiting child"
+#error	Error conditions.	"Premature end of script headers"
+#info	Informational.	"Server seems busy, (you may need to increase StartServers, or Min/MaxSpareServers)..."
+#debug	Debug-level messages	"Opening config file ..."
+LogLevel = ['error', 'info', 'debug']
 
 
 
